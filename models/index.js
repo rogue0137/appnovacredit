25865483d7f8299246b0748d48ac76f275d14c56
Krystal Flores <krystal.flores@gmail.com>
2019-11-17 20:18:01 -0800

    Remove config file from models/index.js

--- models/index.js
+++ models/index.js
@@ -6,16 +6,18 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+let sequelize = new Sequelize(process.env.DATABASE_URL, {
+  "use_env_variable": process.env.DATABASE_UR,
+  "dialect": "postgresql",
+  "ssl":true,
+  "dialectOptions":{
+    "ssl":{
+      "require":true
+    }
+  }
+});
 
 fs
   .readdirSync(__dirname)
